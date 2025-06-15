997947af406b5a0e222121ec135915c0dcc28502
Vijay8143 <194266580+Vijay8143@users.noreply.github.com>
2025-06-15 11:40:08 +0530

    menu bar color correction

--- fadein-animation-menubutton/script.js
+++ fadein-animation-menubutton/script.js
@@ -1,9 +1,21 @@
 const menuBtn = document.getElementById("menu-button");
 const sidebar = document.getElementById("sidebar");
+const bar=document.querySelectorAll(".bar");
 
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open");
+    if(sidebar.classList.contains("open")){
+    bar.forEach((element)=>{
+        element.style.backgroundColor="white";
+    })
+}
+else{
+        bar.forEach((element)=>{
+        element.style.backgroundColor="black";
+    })
+    }
 });
+
 window.addEventListener("click", (event) => {
     if (
     sidebar.classList.contains("open") &&
@@ -11,5 +23,8 @@ window.addEventListener("click", (event) => {
     !menuBtn.contains(event.target)
     ) {
     sidebar.classList.remove("open");
+    bar.forEach((element)=>{
+        element.style.backgroundColor="black";
+    })
     }
 });
